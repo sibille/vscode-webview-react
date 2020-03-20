535c74504d2642ed0361e8ce768368206e83645e
sibille <sibille@ilitia.com>
2020-03-20 18:37:07 +0100

    First cleanup

--- client/src/containers/Footer/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from "react-intl";
-
-const messages = defineMessages({
-  navAriaLabel: {
-    id: "footer.navAriaLabel",
-    defaultMessage: "Navigate between pages and create project"
-  }
-});
-export default messages;
